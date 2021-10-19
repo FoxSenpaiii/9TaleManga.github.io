8c5bc28e89b2ae4d8fbd9f5a1f78f6bca034a332
FoxSenpaiii <82935573+FoxSenpaiii@users.noreply.github.com>
2021-10-19 22:47:47 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,137 @@
+<html>
+    <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href='./css/style.css'/>
+        <script></script>
+        <title>Первый сайт</title>
+    </head>
+    <body>
+       <?php
+            include "./main_pages/main.php";
+       ?>
+       <div class="topnav">
+  <a class="active" href="#home">Главная</a>
+  <a href="#comics">Комиксы</a>
+  <a href="#manga">Манга и Ранобэ</a>
+  <a href="#manhva">Манхва</a>
+  <a href="#about">О Нас</a>
+</div>
+<div class="album py-5 bg-light">
+    <div class="container">
+    <div class="ABZ">
+    <h1 class ="ABZ">Сейчас в тренде <h1>
+</div>
+
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="col">
+          <div class="card shadow-sm">
+          <img src="https://i.pinimg.com/originals/2d/1d/13/2d1d13431711d1fac8dc6b33f27b1b73.jpg" style="width:100%">
+          <div class="ABD">
+    <h1 class ="ABD">Человек-бензопила<h1>
+</div>
+
+
+            <div class="card-body">
+              <p class="card-text">«Я всегда мечтал жить обычной жизнью: спать в тёплой постели, есть тосты с джемом по утрам, ходить на свидания со своей девушкой и улыбаться каждый день. Но всё изменилось со смертью отца — теперь, Потита, пора убивать!» — с такими словами Дэндзи вместе со своим псом-бензопилой Потитой отправляется на очередной контракт, ведь они — охотники на демонов. Каждый день они убивают ради денег, которые Дэндзи должен отдать одному якудза, иначе долг покойного отца придётся отдать собственной жизнью. Но что ждёт Дэндзи, когда он вернёт весь долг: заживёт обычной жизнью или продолжит спасать мир от демонов? А может, у судьбы свои планы на участь героя?<br><br><br>                                                                                </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                <a href="https://mangalib.me/chainsaw-man?section=info" class="sliding-button">Читать онлайн</a>
+                <a href="" class="sliding-button2">Скачать</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          <img src="https://xlm.ru/storage/uploads/images/2021/06/23/sQVTKFGjleOrj0Y9Hh8DxiPqdxfPaq235Va1zw1s.jpeg" style="width:100%">
+          <div class="ABD">
+    <h1 class ="ABD">Волчица и прянности<h1>
+</div>
+            <div class="card-body">
+              <p class="card-text">Крафт Лоуренс — двадцатипятилетний странствующий торговец, мечтающий накопить денег на обустройство собственного магазина. Вот уже семь лет, как он путешествует по городам, набираясь опыта в торговле. Однажды он встречает языческое божество, волчицу Холо, которой более 600 лет. Она выглядит, как юная девушка с волчьими ушами и хвостом, и представляется божеством-покровительницей урожая. Холо многие годы провела в деревне Пасро, но прогресс не стоит на месте, и местные жители перестали нуждаться в её покровительстве. Поэтому волчица решает вернуться в родные места, а Лоуренс ей в этом помогает, сопровождая на север, на её родину.  <br><br><br>                                                                                           </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                <a href="https://mangalib.me/ookami-to-koushinryou?section=info" class="sliding-button">Читать онлайн</a>
+                <a href="" class="sliding-button2">Скачать</a>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          <img src="https://i.pinimg.com/736x/95/a3/1e/95a31e0e756e7dc5aabec912a2cff0f8--light-novel-poster.jpg" style="width:100%">
+          <div class="ABD">
+    <h1 class ="ABD">Реинкарнация безработного<h1>
+</div>
+
+            <div class="card-body">
+              <p class="card-text">34-летний отаку-затворник был выгнан из дома своей семьёй. Этот жирный и уродливый девственник без гроша в кармане осознал, что его жизнь подошла к концу. Затем он понял, что его жизнь вообще могла бы быть намного лучше, если бы он мог избавиться от тёмных страниц своей жизни. Пребывая в сожалениях, он увидел, как грузовик со спящим водителем быстро надвигается на трёх школьников. Собрав все свои силы, он попытался спасти их и погиб.И затем, когда он открыл глаза, он уже переродился в мире меча и магии, как Рудеус Грейрат. Родившись в новом мире, начав новую жизнь, Рудеус решил, что "в этот раз, я проживу свою жизнь на полную, безо всяких сожалений!"</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                <a href="https://mangalib.me/mushoku-tensei-isekai-ittara-honki-dasu?section=info" class="sliding-button">Читать онлайн</a>
+                <a href="" class="sliding-button2">Скачать</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          <img src="https://www.previewsworld.com/SiteImage/FBCatalogImage/STL161663.jpg" style="width:100%">
+          <div class="ABD">
+    <h1 class ="ABD">У Коми сан проблемы с общениием<h1>
+</div>
+
+            <div class="card-body">
+              <p class="card-text">Коми-сан настолько красива и очаровательна, что никто не может отвести от неё взгляд. Почти вся школа считает её холодной красавицей, но Тадано Хитохито знает правду: на самом деле Коми-сан испытывает большие неудобства при общении с людьми. Коми-сан желает избавиться от этой плохой привычки, а Тадано-кун поможет ей в этом.<br><br><br><br><br><br><br><br><br><br><br><br></p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                <a href="https://mangalib.me/miss-komi-is-bad-at-communication?section=info" class="sliding-button">Читать онлайн</a>
+                <a href="" class="sliding-button2">Скачать</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          <img src="https://static2.mangapoisk.ru/pages/3575/209461/Lt1iSkFYt5jtYIjpPqhEaxmwCT9qOn12ze7ov0NY.jpg" style="width:100%">
+          <div class="ABD">
+    <h1 class ="ABD">JoJo Bizzare Adventures: SteelBallRun<h1>
+</div>
+
+            <div class="card-body">
+              <p class="card-text">1890 год. Организатор Стивен Стил устраивает величайшую по своему размаху гонку: начинается она на пляже Сан-Диего, а финиш расположен в самом Нью-Йорке. Призовой фонд - целых 50 миллионов долларов! Наездники со всего мира собираются у стартовой точки соревнования, и каждый по собственной причине. Но и сама гонка не так проста на первый взгляд, как может показаться, ведь и она - прикрытие для некого скрытого замысла...<br><br><br><br><br><br><br><br><br></p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                <a href="https://mangalib.me/jojo-no-kimyou-na-bouken-part-7-steel-ball-run-solored?section=info" class="sliding-button">Читать онлайн</a>
+                <a href="" class="sliding-button2">Скачать</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          <img src="https://cdn.gudangkomik.com/fire-brigade-of-flame/thumb.jpg" style="width:100%">
+          <div class="ABD">
+    <h1 class ="ABD">Пламенная бригада пожарных<h1>
+</div>
+
+            <div class="card-body">
+              <p class="card-text">Люди стали очень часто умирать от самовозгорания. Причины этого феномена неизвестны. К тому же, люди не просто умирают, а превращаются в огнелюдей — безмозглых монстров, которые становятся причиной ещё больших пожаров. Для борьбы с ними были созданы несколько пожарных отрядов, которые, кроме ликвидации огнелюдей, пытаются выяснить, из-за чего это всё происходит. Именно к одному из таких отрядов и присоединяется главный герой — Кусакабэ Шинра. По некоторым причинам его считают повинным в смерти собственной семьи, и чтобы доказать обратное, он хочет стать героем, которого будут уважать и не бояться. Постепенно набираясь опыта, обретая новых друзей, Шинра вместе с членами его отряда будет пробиваться через все преграды, чтобы раскрыть зловещую тайну феномена самовозгорания…</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                <a href="https://mangalib.me/enen-no-shouboutai?section=info" class="sliding-button">Читать онлайн</a>
+                <a href="" class="sliding-button2">Скачать</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
